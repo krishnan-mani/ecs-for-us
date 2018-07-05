a8aa5d437590d1f6f536745334a02ca82c510b90
Krishnan Mani <k.mani.1@elsevier.com>
2018-07-05 07:20:54 +0100

    Next application version

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ app = FlaskAPI(__name__)
 
 @app.route('/example/')
 def example():
-    return {'hello': 'world'}
+    return {'hello': 'world', 'workshop': 'ecs'}
 
 if __name__ == "__main__":
         app.run(debug=True,host='0.0.0.0')

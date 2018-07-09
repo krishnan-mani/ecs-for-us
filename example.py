42ba2099f3610f4896b3136fa3cf962dbd001804
Krishnan Mani <km@krishnanm.com>
2018-07-09 07:49:50 +0100

    Fixed broken code

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ app = FlaskAPI(__name__)
 
 @app.route('/example/')
 def example():
-    return {'hello': 'world', 'workshop': 'ecs', 'bump': true}
+    return {'hello': 'world', 'workshop': 'ecs', 'bump': 'yes'}
 
 if __name__ == "__main__":
         app.run(debug=True,host='0.0.0.0')
